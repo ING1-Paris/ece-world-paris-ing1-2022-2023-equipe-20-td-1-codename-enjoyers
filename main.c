4b3b705e243966d6a22f5188a4bf013cdc9f30f0
Guicha <48026427+Guicha@users.noreply.github.com>
2023-04-28 12:00:39 +0200

    Ajout de la carte du menu principal et début d'implémentation des différents sous programmes. Il faudrait optimiser/corriger un peu le programme de tir aux ballons. Je n'ai pas encore inclus ses sous programmes dans le header en attendant qu'ils soient corrigés. Création du dossier assets avec dedans les différentes bitmaps qu'on utilise dans le programme

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 
 int main() {
 
+    srand(time(NULL));
+
     allegro_init();
 
     install_keyboard();
@@ -9,18 +11,57 @@ int main() {
 
     set_window_title("SHREK MANIA WORLD");
 
+
+    // CREATION DU MODE GRAPHIQUE
+    // -----------------------------
     set_color_depth(desktop_color_depth());
 
-    if (set_gfx_mode(GFX_AUTODETECT_WINDOWED,1000,800,0,0) != 0) {
+    if (set_gfx_mode(GFX_AUTODETECT_WINDOWED,960,720,0,0) != 0) {
 
         allegro_message("Probleme de mode graphique :/");
 
         allegro_exit();
         exit(EXIT_FAILURE);
     }
+    // -----------------------------
 
 
-    allegro_message("Allegro marche !");
+    // INITIALISER LES VARIABLES ICI
+    // -----------------------------
+
+    BITMAP * page; // BITMAP de la page (double buffer)
+    BITMAP * menu;
+
+    // -----------------------------
+
+
+
+
+
+    // CHARGEMENT DES VARIABLES
+    // -----------------------------
+
+    menu = load_bitmap("../assets/maps/menu.bmp", NULL);
+
+    // -----------------------------
+
+
+
+
+
+    // DEBUT DU JEU
+    page = create_bitmap(SCREEN_W, SCREEN_H);
+    clear_bitmap(page);
+
+    blit(menu, page, 0, 0, 0, 0, SCREEN_W, SCREEN_H);
+
+    blit(page, screen, 0, 0, 0, 0, SCREEN_W, SCREEN_H);
+
+    readkey();
+
+
+    destroy_bitmap(page);
+    destroy_bitmap(menu);
 
     allegro_exit();
 
